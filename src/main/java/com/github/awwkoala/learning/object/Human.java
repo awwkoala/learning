458b05446e542285bf1d6a471970654d5bf23484
kmatylewska <karolinam.m@wp.pl>
2021-07-13 16:35:05 +0200

    Review fixes

--- src/main/java/com/github/awwkoala/learning/object/Human.java
+++ src/main/java/com/github/awwkoala/learning/object/Human.java
@@ -1,5 +1,8 @@
 package com.github.awwkoala.learning.object;
 
+import lombok.Getter;
+
+@Getter
 public class Human {
   //  Utwórz klasę Human reprezentującą człowieka, musi posiadać atrybuty takie jak wiek, waga, wzrost, imię i płeć.
 //  Klasa powinna także zawierać metody getAge, getWeight, getHeight, getName, isMale.
@@ -18,26 +21,6 @@ public class Human {
     this.height = height;
     this.name = name;
     this.male = male;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public int getWeight() {
-    return weight;
-  }
-
-  public int getHeight() {
-    return height;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public boolean isMale() {
-    return male;
   }
 
 }
